19a250f66a4cbb6dd4649a606ae58c2940c7a326
Vanshmehta38 <vanshmehta7403@gmail.com>
2024-10-02 13:18:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApplication
+{
+    interface IVehicle
+    {
+        void Start();
+        void Stop();
+    }
+
+    class Car: IVehicle
+    {
+        public void Start()
+        {
+            Console.WriteLine("Car Started.");
+        }
+
+        public void Stop()
+        { 
+            Console.WriteLine("Car Stooped.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Car myCar = new Car();
+            myCar.Start();
+            myCar.Stop();
+
+        }
+    }
+}
